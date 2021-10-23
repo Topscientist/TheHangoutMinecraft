2904846657d489b0474f18c844a287b923027daf
Topscientist <74202229+Topscientist@users.noreply.github.com>
2021-10-23 18:18:17 +0000

    23.10.21 - Backup

--- mc.sh
+++ mc.sh
@@ -77,15 +77,32 @@ sa - South America (Sao Paulo)
 jp - Japan (Tokyo)
 in - India (Mumbai)" 
 
+# start web server
+#echo "Starting web server..."
+echo "Minecraft server starting, please wait" > $root/ip.txt
+
+# start tunnel
 mkdir -p ./logs
 touch ./logs/temp # avoid "no such file or directory"
 rm ./logs/*
 echo "Starting ngrok tunnel in region $ngrok_region"
 ./ngrok authtoken $ngrok_token
 touch logs/ngrok.log
-./ngrok tcp -region eu 2556
+./ngrok tcp -region $ngrok_region --log=stdout 1025 > ./logs/ngrok.log &
+# wait for started tunnel message, and print each line of file as it is written
+tail -f ./logs/ngrok.log | sed '/started tunnel/ q'
+orig_server_ip=`curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[0].public_url'`
+trimmed_server_ip=`echo $orig_server_ip | grep -o '[a-zA-Z0-9.]*\.ngrok.io[0-9:]*'`
+server_ip="${trimmed_server_ip:-$orig_server_ip}"
+echo "Server IP is: $server_ip"
+echo "Server running on: $server_ip" > $root/ip.txt
+
 touch logs/latest.log
+# Experiment: Run http server after all ports are opened
+#( tail -f ./logs/latest.log | sed '/RCON running on/ q' && python3 -m http.server 8080 ) &
+
+# Start minecraft
 PATH=$PWD/jre/bin:$PATH
 echo "Running server..."
-java -jar server.jar nogui
+java -Xmx1G -Xms1G -jar server.jar nogui
 echo "Exit code $?"
